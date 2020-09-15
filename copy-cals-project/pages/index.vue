861489b6ebc463a3ea0cb68e91e3d4e785dbdafd
원수현 <shwon319@daum.net>
2020-09-15 15:17:41 +0900

    commit

--- copy-cals-project/pages/index.vue
+++ copy-cals-project/pages/index.vue
@@ -1,23 +1,7 @@
 <template>
   <section class="container">
     <div>
-      <app-logo/>
-      <h1 class="title">
-        copy-cals-project
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
+      hello world
     </div>
   </section>
 </template>
